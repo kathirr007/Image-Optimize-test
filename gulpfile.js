15d437920517dda2e3d2440ddff4dbe6890981b9
kathirr kannan <kathirr007@gmail.com>
2019-09-13 02:11:00 +0530

    Added impress js for presentation

--- gulpfile.js
+++ gulpfile.js
@@ -22,9 +22,80 @@ let source = './';
 
 let dest = devBuild ? 'builds/development/' : 'builds/production/';
 
+let html = {
+    partials: [source + '_partials/**/*'],
+    in: [source + '*.html'],
+    watch: ['*.html', '_partials/**/*'],
+    out: dest,
+    context: {
+        devBuild: devBuild
+    }
+};
+
+
 let images = {
     in: [source + 'lbd/img/**/*'],
     out: dest + 'lbd/img/'
+};
+
+let css = {
+    in: [source + 'sass/style.scss'],
+    watch: ['sass/**/*.scss', '!sass/b2b/**/*'],
+    out: dest + 'css/',
+    pluginCSS: {
+        in: [
+            source + 'node_modules/bootstrap/scss/bootstrap.scss',
+            source + 'node_modules/slick-carousel/slick/slick.css',
+            source + 'node_modules/slick-carousel/slick/slick-theme.css',
+            source + 'node_modules/simplebar/dist/simplebar.min.css',
+        ],
+        watch: ['css/**/*.css'],
+        out: dest + 'css/'
+    },
+    sassOpts: {
+        outputStyle: devBuild ? 'compressed' : 'compressed',
+        imagePath: '../images',
+        precision: 3,
+        errLogToConsole: true
+    }
+};
+
+let fonts = {
+    in: source + 'fonts/**/*',
+    out: dest + 'fonts/'
+};
+
+let js = {
+    in: [
+        // source + 'js/jquery-3.1.1.min.js',
+        source + 'node_modules/jquery/dist/jquery.min.js',
+        // source + 'js/bootstrap-history-tabs.js',
+        // source + 'js/bootstrap-tabs-history.js',
+        // source + 'js/jquery.stickytabs.js',
+        source + 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+        source + 'node_modules/slick-carousel/slick/slick.min.js',
+        source + 'node_modules/simplebar/dist/simplebar.min.js',
+        source + 'lbd/js/impress.js',
+        // source + 'js/custom.js'
+    ],
+    out: dest + 'js/'
+};
+
+let jsLibs = {
+    in: source + 'lib/**/*',
+    watch: 'lib/**/*',
+    out: dest + 'lib/'
+};
+
+let syncOpts = {
+    server: {
+        baseDir: dest,
+        index: 'index.html'
+    },
+    open: false,
+    injectChanges: true,
+    reloadDelay: 0,
+    notify: true
 };
 
 
@@ -37,10 +108,50 @@ gulp.task('clean-images', cb => {
     del([dest + 'lbd/img/**/*'], cb());
 });
 
+gulp.task('clean-html', () => {
+    return del([dest + '**/*.html']);
+});
+
+gulp.task('clean-css', cb => {
+    del([dest + 'css/**/*'], cb());
+});
+
+gulp.task('clean-js', cb => {
+    del([dest + 'js/**/*'], cb());
+});
+
+gulp.task('clean-jslib', cb => {
+    del([dest + 'lib/**/*'], cb());
+});
+
 // reload task
 gulp.task('reload', done => {
     browserSync.reload();
     done();
+});
+
+// build HTML files
+gulp.task('html', () => {
+    var page = gulp
+        .src(html.in)
+        .pipe($.newer(html.out))
+        .pipe($.preprocess({
+            context: html.context
+        }));
+    /*.pipe($.replace(/.\jpg|\.png|\.tiff/g, '.webp'))*/
+    if (!devBuild) {
+        page = page
+            .pipe($.size({
+                title: 'HTML in'
+            }))
+            .pipe($.htmlclean())
+            .pipe($.size({
+                title: 'HTML out'
+            }));
+    }
+    return (
+        page.pipe(gulp.dest(html.out))
+    );
 });
 
 // manage images
@@ -96,6 +207,229 @@ gulp.task('optim-images', function () {
 });
 
 
+// copy fonts
+gulp.task('fonts', () => {
+    return gulp
+        .src(fonts.in)
+        .pipe($.newer(dest + 'fonts/'))
+        .pipe(gulp.dest(dest + 'fonts/'));
+});
+
+// plugin css compilation
+gulp.task(
+    'css',
+    gulp.series('fonts', () => {
+        var cssFilter = $.filter(['**/*.css'], {
+                restore: true
+            }),
+            imageFilter = $.filter(['**/*.+(jpg|png|gif|svg)'], {
+                restore: true
+            }),
+            imageFilter2 = $.filter(['**/*.+(jpg|png|tiff|webp)'], {
+                restore: true
+            });
+        return (
+            gulp
+            .src(css.pluginCSS.in, {allowEmpty: true})
+            .pipe($.size({
+                title: 'CSS in '
+            }))
+            .pipe($.newer(dest + 'css/'))
+            // .pipe(cssFilter)
+            .pipe(
+                $.rename(function (path) {
+                    path.extname = '.scss';
+                })
+            )
+            .pipe($.sourcemaps.init())
+            .pipe($.plumber())
+            .pipe($.sass(css.sassOpts))
+            .pipe($.cssnano({zindex: false}))
+            .pipe($.csso())
+            .pipe($.cleanCss({level: {2: {all: true}}}))
+            .pipe(
+                $.order([
+                    'bootstrap.css',
+                    'bootstrap-select.min.css',
+                    'bootstrap-datetimepicker.min.css',
+                    'slick.css',
+                    'slick-theme.css',
+                    'pretty-checkbox.min.css',
+                    'lightgallery.min.css',
+                    'simplebar.min.css'
+                ])
+            )
+            /* .pipe($.concatCss('lbd-bundle.css', {
+                rebaseUrls: false
+            })) */
+            .pipe($.groupConcat({
+                'plugins-bundle.css': '**/*.css'
+            }))
+            .pipe($.sourcemaps.write('./maps'))
+            // .pipe(cssFilter.restore)
+            .pipe($.size({
+                title: 'CSS out '
+            }))
+            .pipe(gulp.dest(dest + 'css/'))
+            .pipe(browserSync.stream({
+                match: '**/*.css'
+            }))
+        );
+    })
+);
+
+// sass compilation
+gulp.task('sass', function(){
+    return gulp.src(css.in, {allowEmpty : true})
+    .pipe($.size({
+        title: 'SCSS in '
+    }))
+    .pipe($.sourcemaps.init())
+    .pipe($.plumber())
+    .pipe($.sass(css.sassOpts))
+    .pipe($.autoprefixer({
+        // browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe($.cssnano({zindex: false}))
+    .pipe($.csso())
+    .pipe($.cleanCss({level: {2: {all: true}}}))
+    .pipe($.size({
+        title: 'SCSS out '
+    }))
+    .pipe($.sourcemaps.write('./maps'))
+	.pipe(gulp.dest(css.out))
+    .pipe(browserSync.stream({
+        match: '**/*.css'
+    }));
+});
+
+// js tasks
+gulp.task('js', () => {
+    var jsFilter = $.filter(['**/*.js', '!**/*custom.js'], {
+        restore: true
+    });
+    if (devBuild) {
+        return (
+            gulp
+            .src(js.in)
+            .pipe($.size({
+                title: 'JS in '
+            }))
+            .pipe($.newer(dest + 'js/'))
+            .pipe(jsFilter)
+            .pipe($.babel({
+                presets: [
+                  ['@babel/env', {
+                    modules: false
+                  }]
+                ]
+              }))
+            .pipe($.sourcemaps.init())
+            .pipe($.uglify())
+            .pipe(
+                $.order([
+                    'jquery-3.1.1.min.js',
+                    'bootstrap.bundle.min.js',
+                    'popper.min.js',
+                    'slick.min.js',
+                    'bootstrap-select.min.js',
+                    'simplebar.min.js',
+                    'moment.min.js',
+                    'bootstrap-datetimepicker.min.js',
+                    'lightgallery-all.min.js',
+                    'jquery.bootstrap.wizard.js',
+                    'prettify.js',
+                    // 'bootstrap-history-tabs.js',
+                    // 'bootstrap-tabs-history.js',
+                    // 'jquery.stickytabs.js',
+                    // "lbd/js/custom.js"
+                ])
+            )
+            .pipe($.groupConcat({
+                'js-bundle.js': '**/*.js'
+            }))
+            .pipe($.sourcemaps.write('./maps'))
+            .pipe(jsFilter.restore)
+            .pipe($.size({
+                title: 'JS out '
+            }))
+            .pipe(gulp.dest(dest + 'js/'))
+        );
+    }
+});
+
+gulp.task('jslib', function () {
+    var toExclude = ['lbd/lib-live/tinymce_4.2.5/**/*'],
+        htmlFilter = $.filter(['**/*.html', '**/*.md'], {
+            restore: true
+        }),
+        includeIgnoredJs = $.filter(toExclude[0] + '.js', {
+            restore: true
+        }),
+        includeIgnoredCss = $.filter(toExclude[0] + '.css', {
+            restore: true
+        }),
+        cssFilter = $.filter(['**/*.css'], {
+            restore: true
+        }),
+        imageFilter = $.filter(['**/*.+(jpg|png|gif|svg)'], {
+            restore: true
+        }),
+        imageFilter2 = $.filter(['**/*.+(jpg|png|tiff|webp)'], {
+            restore: true
+        }),
+        jsonFilter = $.filter(['**/*.json'], {
+            restore: true
+        }),
+        jsFilter = $.filter(['**/*.js'], {
+            restore: true
+        });
+
+    if (devBuild) {
+        return (
+            gulp
+            .src(jsLibs.in)
+            .pipe($.size({
+                title: 'jsLibs in '
+            }))
+            .pipe($.newer(jsLibs.out))
+            .pipe(jsFilter)
+            .pipe($.uglify())
+            .on('error', function (err) {
+                $.util.log($.util.colors.red('[Error]'), err.toString());
+            })
+            .pipe(jsFilter.restore)
+            .pipe(cssFilter)
+            .pipe(
+                $.rename(function (path) {
+                    path.extname = '.scss';
+                })
+            )
+            .pipe($.plumber())
+            .pipe($.sass(css.sassOpts))
+            .pipe($.cssnano({zindex: false}))
+            .pipe($.csso())
+            .pipe($.cleanCss({level: {2: {all: true}}}))
+            .pipe(cssFilter.restore)
+            .pipe(htmlFilter)
+            .pipe($.htmlclean())
+            .pipe(htmlFilter.restore)
+            .pipe(imageFilter)
+            .pipe($.image({
+                jpegRecompress: ['--strip', '--quality', 'medium', '--min', 40, '--max', 80],
+                gifsicle: ['--optimize'],
+                quiet: true
+            }))
+            .pipe(imageFilter.restore)
+            .pipe($.size({
+                title: 'jsLibs out '
+            }))
+            .pipe(gulp.dest(jsLibs.out))
+        );
+    }
+});
+
 // browser sync
 gulp.task('serve', () => {
 
@@ -116,8 +450,26 @@ gulp.task('serve', () => {
 gulp.task(
     'watch',
     gulp.parallel('serve', () => {
+        // html changes
+        gulp.watch(html.watch, gulp.series('html', 'reload'));
         // image changes
         gulp.watch(images.in, gulp.series('images'));
+
+        // font changes
+        gulp.watch(fonts.in, gulp.series('fonts'));
+
+        // sass changes
+        gulp.watch(css.watch, gulp.series('sass'));
+
+        // pluginCSS changes
+        gulp.watch([...css.pluginCSS.in, source + 'node_modules/bootstrap/scss/**/*.scss'], gulp.series('css'));
+
+        // javascript changes
+        gulp.watch(js.in, gulp.series('js', 'reload'));
+
+        // javascript libraries changes
+        gulp.watch(jsLibs.in, gulp.series('jslib', 'reload'));
+        // gulp.watch(jsLibs.in, [ reload]);
 
     })
 );
@@ -126,7 +478,7 @@ gulp.task(
 gulp.task(
     'default',
     gulp.parallel(
-        'images',
+        'html', 'css', 'sass', 'images', 'js', 'jslib',
         gulp.series('watch')
     )
 );
